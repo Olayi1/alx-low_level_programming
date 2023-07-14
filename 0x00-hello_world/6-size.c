76e1a294d104841758f7bd05f2083d4965003a95
Olayi1 <olayinkagab@gmail.com>
2023-07-14 04:49:06 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
-*Descrption: print size of a
+*Descrption:print
 *
-*Return: 0 always (sucess)
+*Return: 0 always(sucess)
 */
-int main(void); 
-{char c;
-int a;
-long int b;
-long long int c;
+int main(void);
+{char a;
+int b;
+long int c;
+long long int d;
 float f;
-printf("size of a:%lu byte(s)\n", (unsignedlong) sizeof(c));
-printf("size of a:%lu byte(s)\n", (unsignedlong) sizeof(a));
-printf("size of a:%lu byte(s)\n", (unsignedlong) sizeof(b));
+printf("size of a:%lu byte(s)\n", (unsignedlong)sizeof(a));
+printf("size of a:%lu byte(s)\n", (unsignedlong)sizeof(b));
+printf("size of a:%lu byte(s)\n", (unsignedlong)sizeof(c));
+printf("size of a:%lu byte(s)\n", (unsignedlong)sizeof(d));
 printf("size of a:%lu byte(s)\n", (unsignedlong) sizeof(f));
 return (0);
 }
