a7fada257b2fe0f37a399413083f6ec48c852ab9
Olayi1 <olayinkagab@gmail.com>
2023-07-14 03:26:03 -0700

    ko

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  *Return: Always 0 (success)
  */
 int main(void)
-{ printf("with proper grammar\, but the outcome is a piece of art\,\n);
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
