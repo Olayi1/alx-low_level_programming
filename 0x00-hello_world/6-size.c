89b281c7fb8e1c35d579d7f324c25a7be09f125c
Olayi1 <olayinkagab@gmail.com>
2023-07-14 05:46:00 -0700

    ko

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,6 +21,6 @@ printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
 printf("size of along long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+printf("size of a float: %lu byte(s)", (unsigned long) sizeof(f));
 return (0);
 }
