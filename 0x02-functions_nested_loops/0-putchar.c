da5d3b8abadfc7e9c3c3f0b404607e59b4d02440
Olayi1 <olayinkagab.com>
2023-07-18 23:22:02 +0100

    void

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-*main -print putchar.
+*main - starting point.
 *
 *Return: (0)
 */
+
 int main(void)
 {
-	putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main"
+/**
+*main - check code.
+*Return: (0)
+*/
+int main(void)
+{
+print_alphabet("abcdefghijklmnopqrstuvwxyz\n");
+    return (0);
+}
