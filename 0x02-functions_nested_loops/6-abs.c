0903712e32dd1c2918cd7267e7e18a9feeec7c6e
Olayi1 <olayinkagab.com>
2023-07-19 16:30:27 +0100

    jk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_abs - fuction prototype.
+*
+*@a: function added
+*
+*Return:a or a
+*/
+
+int _abs(int a)
+{
+if (a > 0)
+return (-a);
+else
+{
+if (a <= 0)
+return (a);
+}
+return (0);
+}
